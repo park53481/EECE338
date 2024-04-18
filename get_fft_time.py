62b3f7ed54d9ce99033ba48a1bb3e18bf02f668d
park53481 <167415953+park53481@users.noreply.github.com>
2024-04-18 18:25:19 +0900

    Update get_fft_time.py

--- get_fft_time.py
+++ get_fft_time.py
@@ -2,17 +2,22 @@ import numpy as np
 import scipy.io as sio
 import scipy.io.wavfile
 import matplotlib.pyplot as plt
-import argparse 
-from scipy import fftpack
+import argparse
+ 
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', required=False, default='filename.wav')
 args = parser.parse_args()
 
+time_1= time()
+
 print("drawing plot for", args.filename)
+
+time_2= time()
 
 samplerate, data = sio.wavfile.read(args.filename)
 fftsize = len(data)
 data_fft = fftpack.fft(data, fftsize)
 
-# fast fourier transform 
+print('fft_time= ', time_2-time_1)
